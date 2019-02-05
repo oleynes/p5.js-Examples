367e9088005fbd9cec07a0f35c3abfda01ed2ec0
Owen Leynes <oleynes@gmail.com>
2019-02-05 16:55:38 -0500

    Added Chapters 1-3

---
+++ Chapter 1+2/template.js
@@ -0,0 +1,45 @@
+/**
+*   @author "Your Name"
+*   @version 1.0
+*
+******************************************************
+* Description:
+*
+*
+*
+******************************************************
+* Credits:
+*
+*
+*
+*/
+
+
+//Global Variables
+
+
+
+
+
+//preload function (if needed)
+//function preload(){}
+
+//Setup Function:
+function setup(){
+
+}
+
+//Draw Function:
+function draw(){
+
+}
+
+//Auxilary Functions:
+
+
+
+
+
+
+//////////////////////////
+//Classes
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # p5.js-Examples
 ### Based on 'Getting Started with p5.js' by Lauren McCarthy and Ben Fry
 
-##Contents:
+## Contents:
+-Chapter 1+2:
+  -Sketch Template
+  -Offline Setup Walk-through
+-Chapter 3:
+  -
