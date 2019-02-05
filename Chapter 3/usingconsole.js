5dcbeaace385b47ee483d12b2c450ec6c0b42bfb
Owen Leynes <oleynes@gmail.com>
2019-02-05 17:18:50 -0500

    Updated usingconsole.js

--- Chapter 3/usingconsole.js
+++ Chapter 3/usingconsole.js
@@ -4,7 +4,24 @@
 *
 *   Some examples of using the console for debugging
 */
+//Set this to false to disable console outputs
+var DEBUG = true;
 
 function setup(){
-  createCanvas()
+  //Create Canvas
+  createCanvas(800, 600);
+  if(DEBUG){
+    print("Canvas Dimentions: width = " + width + ", height = " + height);
+    print("Center: " + width/2 + ", " + height/2);
+  }
+  background(0);
+}
+
+function draw(){
+  //If mouse is pressed and debug is true, output current mouse coords
+  if(mouseIsPressed && DEBUG){
+    print("X: " + mouseX + " Y: " + mouseY);
+  }
+
+
 }
